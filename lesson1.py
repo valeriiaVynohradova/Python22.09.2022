4e5151b57519efe4bb6549d8862b361da12c0787
valeriiaVynohradova <valeron0429@gmail.com>
2022-09-30 10:16:13 +0300

    Add file

---
+++ lesson1.py
@@ -0,0 +1,31 @@
+# Задача 1. Створіть дві змінні first=10, second=30.
+# Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+first = 10
+second = 30
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+print(first // second)
+print(first ** second)
+print(first % second)
+
+# Задача 2. Створіть змінну і запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1.
+# Виведіть на екран результат кожного порівняння.
+comparison_result = first > second
+print(comparison_result)
+
+comparison_result = first < second
+print(comparison_result)
+
+comparison_result = first == second
+print(comparison_result)
+
+comparison_result = first != second
+print(comparison_result)
+
+# Задача 3. Створіть змінну - результат конкатенації (складання) строк str1="Hello " и str2="world". Виведіть на екран.
+str1 = "Hello "
+str2 = "world"
+result = str1 + str2
+print(result)
